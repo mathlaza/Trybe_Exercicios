c90b0cc947b78533ca9fd5ca3bd84f0cf43e73f5
Matheus Martinez <decretomusic@gmail.com>
2022-05-16 19:32:49 -0300

    exercicios 4.3

---
+++ Fundamentos/bloco-4-introducao-a-javascript/dia_3/scripts.js
@@ -0,0 +1,36 @@
+// //fatorial
+// let fatorial = 10;
+
+// for (let i = fatorial - 1; i > 0; i -= 1) {
+//     fatorial = fatorial * i;
+// }
+// console.log(fatorial);
+
+
+// //inverte palavra
+// let word = 'tryber';
+// let armazena = '';
+
+// for (let i = word.length - 1; i >= 0; i -= 1) {
+//     armazena = armazena + word[i];
+// }
+// console.log(armazena);
+
+
+// //retorna a maior palavra do array
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let counter = 0;
+// let compare = 0;
+// let armazena = 0;
+
+// for (let i = 0; i < array.length; i += 1) {
+//     for (let j = 1; j <= array[i].length; j += 1) {
+//         counter += 1;
+//     }
+//     if (counter > compare) {
+//         compare = counter;
+//         armazena = array[i];
+//     }
+//     counter = 0;
+// }
+// console.log(armazena);

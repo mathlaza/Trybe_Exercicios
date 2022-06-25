58cc9a9bcaeac075905dd99812a81a92e7afa85e
Matheus Martinez <decretomusic@gmail.com>
2022-06-24 22:58:44 -0300

    Exercícios 1 ao 7

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
@@ -0,0 +1,108 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+//1. Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+function authorBornIn1947() {
+  return books.find((book) => book.author.birthYear === 1947).author.name;
+};
+
+//2. Retorne o nome do livro de menor nome.
+function smallerName() {
+  let nameBook;
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  })
+  return nameBook;
+};
+
+//3. Encontre o primeiro livro cujo nome possui 26 caracteres.
+function getNamedBook() {
+  return books.find((book) => book.name.length === 26);
+};
+
+//4. Ordene os livros por data de lançamento em ordem decrescente.
+const releaseYearDec = (books) => {
+  return books.sort((a, b) => b.releaseYear - a.releaseYear);
+};
+
+//5. Faça uma função que retorne true, se todas as pessoas autoras nasceram no século XX, ou false, caso contrário.
+
+const bornCentury = (books) => {
+  return books.every((book) => (book.author.birthYear > 1900 && book.author.birthYear <= 1999));
+};
+
+//6. Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+
+const eightiesLanch = (books) => {
+  return books.some((book) => (book.releaseYear >= 1980 && book.releaseYear < 1990));
+}
+
+//7. Faça uma função que retorne true, caso nenhum author tenha nascido no mesmo ano, e false, caso contrário.
+
+const sameYear = (books) => {
+  return books.every((book) => !books.some((any) => (any.author.birthYear === book.author.birthYear) && (any.author.name !== book.author.name)));
+}
+
+console.log(sameYear(books));
